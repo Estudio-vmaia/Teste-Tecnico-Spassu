b73d18aef6c2644e488266d7aa4e3af9ed061787
renan-redmi <80184338+Estudio-vmaia@users.noreply.github.com>
2025-09-27 00:07:14 -0300

    Alteracao do bootstrap local para o CDN

--- LayoutBase/livros.php
+++ LayoutBase/livros.php
@@ -138,7 +138,7 @@ if (isset($_GET['editar'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gerenciar Livros</title>
-    <link href="vendor/bootstrap/scss/bootstrap.scss" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="css/sb-admin-2.css" rel="stylesheet">
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
     <style>
