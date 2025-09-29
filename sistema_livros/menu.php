f6fdafe7a87fc2ebd7af758644665fb8ec6c66ef
Renan Maia <contato@vmaia.com.br>
2025-09-29 14:45:14 -0300

    htaccess para remover .php da url e passar as conf para o docker

--- sistema_livros/menu.php
+++ sistema_livros/menu.php
@@ -1,26 +1,26 @@
 <!-- Navigation -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-        <a class="navbar-brand" href="index.php">
+        <a class="navbar-brand" href="index">
             <i class="fas fa-book"></i> Sistema de Livros
         </a>
         <div class="navbar-nav ms-auto">
-            <a class="nav-link" href="index.php">
+            <a class="nav-link" href="index">
                 <i class="fas fa-home"></i> Início
             </a>
-            <a class="nav-link" href="livros.php">
+            <a class="nav-link" href="livros">
                     <i class="fas fa-book"></i> Livros
             </a>
-            <a class="nav-link" href="autores.php">
+            <a class="nav-link" href="autores">
                 <i class="fas fa-user-edit"></i> Autores
             </a>
-            <a class="nav-link" href="assuntos.php">
+            <a class="nav-link" href="assuntos">
                 <i class="fas fa-tags"></i> Assuntos
             </a>
-            <a class="nav-link" href="relacionamentos.php">
+            <a class="nav-link" href="relacionamentos">
                 <i class="fas fa-link"></i> Relacionamentos
             </a>
-            <a class="nav-link" href="relatorio.php">
+            <a class="nav-link" href="relatorio">
                 <i class="fas fa-chart-bar"></i> Relatórios
             </a>
         </div>
